bce445f56bd3e80afa3e3f91d288e5a3a70a7f09
Jono Yang <jyang@nexb.com>
2025-07-31 10:25:49 -0700

    Add fastseq source files from sctk branch
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 import setuptools
+from Cython.Build import cythonize
+
+
+extensions = [
+    setuptools.Extension("fastseq._seq", ["src/fastseq/_seq.pyx"], language="c++"),
+]
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(ext_modules=cythonize(extensions))
